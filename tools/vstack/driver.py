a67462b25dda8abd4a3582c60f98baa3c04600e3
Silvio Ortiz <silvioo@science.xyz>
2026-01-17 20:31:13 -0800

    fix vstack imoprt

--- tools/vstack/driver.py
+++ tools/vstack/driver.py
@@ -1,9 +1,10 @@
+import logging
 import os
-import logging 
-import threading 
+import threading
 import time
-from tools.base_server import ABCToolDriver
 from typing import Union
+
+from tools.base_server import ABCToolDriver
 
 try:
     import pythoncom
@@ -13,174 +14,151 @@ except Exception:
 
 if os.name == "nt":
     import clr  # type: ignore
+
     SDK_DLL = os.path.join(
-                os.path.dirname(os.path.abspath(__file__)), "deps",
-                "AxInterop.StackerLib.dll",
-            )
+        os.path.dirname(os.path.abspath(__file__)),
+        "deps",
+        "AxInterop.VSTACKBIONETLib.dll",
+    )
 
-    clr.AddReference(SDK_DLL) 
+    clr.AddReference(SDK_DLL)
 
-    from AxStackerLib import AxStacker  # type: ignore
+    from AxVSTACKBIONETLib import AxVStackBioNet  # type: ignore
 
 else:
 
-    class AxStacker():  # type: ignore
+    class AxVStackBioNet:  # type: ignore
         def __init__(self) -> None:
             pass
-    
+
         def Initialize(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def Close(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def Abort(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def Downstack(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def Home(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def Ignore(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def Jog(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def LoadStack(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def OpenGripper(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def ReleaseStack(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def Retry(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def SetButtonMode(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def SetLabware(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def ShowDiagsDialog(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def Upstack(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
-        
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
+
         def GetLastError(self) -> None:
-            raise NotImplementedError(
-                "AxStacker is not supported on non-Windows platforms"
-            )
+            raise NotImplementedError("AxVStackBioNet is not supported on non-Windows platforms")
 
 
 class VStackDriver(ABCToolDriver):
     def __init__(self, profile: str) -> None:
         self.profile: str = profile
         self.live: bool = False
-        self.client: AxStacker
+        self.client: AxVStackBioNet
         self.lock = threading.Lock()  # To synchronize access to the device
         self.instantiate()
         self.initialize()
 
     def instantiate(self) -> None:
         pythoncom.CoInitialize()
-        self.client = AxStacker()
+        self.client = AxVStackBioNet()
         self.client.CreateControl()
         self.client.Blocking = True
         pythoncom.CoUninitialize()
 
-    def initialize(self) -> None: 
+    def initialize(self) -> None:
         args: dict = {"profile": self.profile}
         self.execute_command("initialize", args)
-    
+
     def close(self) -> None:
         self.schedule_threaded_command("close", {})
-    
+
     def abort(self) -> None:
         self.schedule_threaded_command("abort", {})
-        return 
-    
+        return
+
     def downstack(self) -> None:
         self.schedule_threaded_command("downstack", {})
-        return 
-    
+        return
+
     def home(self) -> None:
         self.schedule_threaded_command("home", {})
-        return 
-    
+        return
+
     def jog(self, increment: float) -> None:
         self.schedule_threaded_command("jog", {"increment": increment})
-        return 
-    
+        return
+
     def load_stack(self) -> None:
         self.schedule_threaded_command("load_stack", {})
-        return 
-    
+        return
+
     def open_gripper(self, open: bool) -> None:
         self.schedule_threaded_command("open_gripper", {"open": open})
-        return 
-    
+        return
+
     def release_stack(self) -> None:
         self.schedule_threaded_command("release_stack", {})
-        return 
-    
+        return
+
     def retry(self) -> None:
         self.schedule_threaded_command("retry", {})
-        return 
-    
+        return
+
     def set_button_mode(self, run_mode: bool, reply: str) -> None:
         self.schedule_threaded_command("set_button_mode", {"run_mode": run_mode, "reply": reply})
-        return 
-    
+        return
+
     def set_labware(self, labware: str, plate_dimension_select: int) -> None:
-        self.schedule_threaded_command("set_labware", {"labware": labware, "plate_dimension_select": plate_dimension_select})
-        return 
-    
+        self.schedule_threaded_command(
+            "set_labware", {"labware": labware, "plate_dimension_select": plate_dimension_select}
+        )
+        return
+
     def show_diagnostics(self) -> None:
         self.schedule_threaded_command("show_diagnostics", {})
-    
+
     def upstack(self) -> None:
         self.schedule_threaded_command("upstack", {})
-        return 
+        return
 
     def schedule_threaded_command(self, command: str, arguments: dict) -> None:  # type: ignore
-        self.execution_thread = threading.Thread(target=self.execute_command(command, arguments,))  # type: ignore
+        self.execution_thread = threading.Thread(
+            target=self.execute_command(
+                command,
+                arguments,
+            )
+        )  # type: ignore
         self.execution_thread.daemon = True
         self.execution_thread.start()
         return None
@@ -213,7 +191,9 @@ class VStackDriver(ABCToolDriver):
             elif command == "set_button_mode":
                 response = self.client.SetButtonMode(arguments["run_mode"], arguments["reply"])
             elif command == "set_labware":
-                response = self.client.SetLabware(arguments["labware"], arguments["plate_dimension_select"])
+                response = self.client.SetLabware(
+                    arguments["labware"], arguments["plate_dimension_select"]
+                )
             elif command == "show_diagnostics":
                 response = self.client.ShowDiagsDialog(True, 0)
             elif command == "upstack":
