37c7b4f9c4c2c7ede9cb8f39bb882f322eeabd83
Alberto Nava <alberton@science.xyz>
2026-02-17 11:39:25 -0800

    add prototype server

---
+++ tools/lcus1_relay/server.py
@@ -0,0 +1,42 @@
+import logging
+
+from tools.base_server import ToolServer, serve
+from tools.grpc_interfaces.lcus1_relay_pb2 import Command, Config
+
+from .driver import Lcus1RelayDriver
+import argparse
+
+
+class Lcus1RelayServer(ToolServer):
+    toolType = "lcus1_relay"
+    driver: Lcus1RelayDriver
+    config: Config
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def _configure(self, config: Config) -> None:
+        self.config = config
+        if self.driver:
+            self.driver.close()
+        self.driver = Lcus1RelayDriver(port=self.config.com_port)
+        self.driver.initialize()
+
+    def Switch(self, params: Command.Switch) -> None:
+        if params.on:
+            self.driver.on()
+        else:
+            self.driver.off()
+
+    def EstimateSwitch(self, params: Command.Switch) -> int:
+        return 1
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--port")
+    args = parser.parse_args()
+    if not args.port:
+        raise RuntimeWarning("Port must be provided...")
+    serve(Lcus1RelayServer(), str(args.port))
